6cbc490845a509b133bf5feea6bf81b9828314b3
shinbm44 <78016592+shinbm44@users.noreply.github.com>
2024-05-27 21:45:28 +0900

    initial commit

---
+++ src/main/java/org/example/jpapractice/JpapracticeApplication.java
@@ -0,0 +1,13 @@
+package org.example.jpapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpapracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpapracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jpapractice/JpapracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jpapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpapracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
